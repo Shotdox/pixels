77082e82bfc1dd0f1f478fd61207485efb8ac3aa
Alexander <alwolski7@gmail.com>
2023-03-10 06:50:03 +1100

    WSAD Movement

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ use fractal::compute_iterations;
 use minifb::{Window, WindowOptions, Key};
 use Vec;
 mod fractal;
+use std::f64::consts::E;
 
 const WIDTH: usize = 851;
 const HEIGHT: usize = 851;
+const MOVE_SPEED: f64 = 20.;
+const ZOOM_SPEED: f64 = 5.;
+const COLOUR_RANGE: i32 = 3;
+const MAX_ITERATIONS: usize = 256;
+
 
 fn from_u8_rgb(r: u8, g: u8, b: u8) -> u32 {
     let (r, g, b) = (r as u32, g as u32, b as u32);
@@ -25,12 +31,12 @@ pub struct Vec2 {
 
 
 impl Vec2{
-    pub fn new(pixel: usize) -> Vec2{
+    pub fn new(pixel: usize, zoom: f64, x_offset:f64, y_offset: f64) -> Vec2{
         Self{
-            original_x: ((pixel % WIDTH) as f64 - (WIDTH/2) as f64)/ 200.,
-            original_y: ((pixel / WIDTH) as f64 - (WIDTH/2) as f64)/ 200.,
-            x: ((pixel % WIDTH) as f64 - (WIDTH/2) as f64)/ 200.,
-            y: ((pixel / WIDTH) as f64 - (WIDTH/2) as f64)/ 200.,
+            original_x: ((pixel % WIDTH) as f64 - (WIDTH/2) as f64)/ zoom + x_offset,
+            original_y: ((pixel / WIDTH) as f64 - (WIDTH/2) as f64)/ zoom + y_offset,
+            x: ((pixel % WIDTH) as f64 - (WIDTH/2) as f64)/ zoom + x_offset,
+            y: ((pixel / WIDTH) as f64 - (WIDTH/2) as f64)/ zoom + y_offset,
             value: 0.,
         }
     }
@@ -53,21 +59,33 @@ impl Vec2{
         }
 
         else{
-            self.value = compute_iterations(&mut Vec2::new_point(0., 0.), self, 124) as f64 * 255. * 255. / 124.;
-            //self.value = compute_iterations(self, &mut Vec2::new_point(0.8 * time.sin().powf(2.), time.sin()), 50) as f64 * 255. * 255. / 50.;
+            self.value = compute_iterations(
+                &mut Vec2::new_point(0., 0.),
+                self,
+                MAX_ITERATIONS)
+                as f64 * (255_f64).powi(COLOUR_RANGE) / MAX_ITERATIONS as f64;
+            //self.value = compute_iterations(self, &mut Vec2::new_point(0.8 * time.sin().powf(2.), time.sin()), MAX_ITERATIONS) as f64 * (255_f64).powi(COLOUR_RANGE) / MAX_ITERATIONS as f64;
         }
         
     }
 }
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
     let mut value_buffer: Vec<Vec2> = Vec::new();
-    for (i, _) in buffer.iter().enumerate(){
-        let mut this_point = Vec2::new(i);
-        this_point.calculate(0 as f64);
-        value_buffer.push(this_point);
+    let mut x_offset = 0.;
+    let mut y_offset = 0.;
+    let mut zoom = 200.;
+
+
+    fn set_buffer(value_buffer: &mut Vec<Vec2>, zoom:f64, x_offset: f64, y_offset: f64){
+        for i in 0..(WIDTH*HEIGHT){
+            let mut this_point = Vec2::new(i, zoom, x_offset, y_offset);
+            this_point.calculate(0 as f64);
+            value_buffer.push(this_point);
+        }
     }
+
+    set_buffer(&mut value_buffer, 200., 0., 0.);
 
     let mut my_window = Window::new(
         "Pixels",
@@ -82,28 +100,50 @@ fn main() {
 
     let mut t = 1;
     while my_window.is_open() && !my_window.is_key_down(Key::Escape){
-        if t % 1 == 0{
-            value_buffer.iter_mut().for_each(|point: &mut Vec2| {
-                point.reset();
-                point.calculate(t as f64 / 100.);
-            });
-
-            my_window.update_with_buffer(&value_buffer.iter().map(|value| value.value as u32).collect::<Vec<u32>>(), WIDTH, HEIGHT).unwrap();
-        }
+        let mut update = false;
 
         if my_window.is_key_down(Key::Space){
             t += 1;
+            update = true;
             println!("Cr: {} Ci: {}", 0.8 * (t as f64).sin().powf(2.), (t as f64).sin())
         }
-        else{
-            thread::sleep(time::Duration::from_micros(16600))
+        if my_window.is_key_down(Key::W){
+            y_offset -= MOVE_SPEED / zoom;
+            update = true;
         }
-        
-        
+        if my_window.is_key_down(Key::S){
+            y_offset += MOVE_SPEED / zoom;
+            update = true;
+        }
+        if my_window.is_key_down(Key::A){
+            x_offset -= MOVE_SPEED / zoom;
+            update = true;
+        }
+        if my_window.is_key_down(Key::D){
+            x_offset += MOVE_SPEED / zoom;
+            update = true;
+        }
+        if my_window.is_key_down(Key::Q){
+            zoom *= ZOOM_SPEED;
+            update = true;
+        }
+        if my_window.is_key_down(Key::E){
+            zoom /= ZOOM_SPEED;
+            update = true;
+        }    
+
+        if update{
+            for i in 0..(WIDTH*HEIGHT){
+                let mut this_point = Vec2::new(i, zoom, x_offset, y_offset);
+                this_point.calculate(t as f64 / 30.);
+                value_buffer[i] = this_point;
+            }
+
+            my_window.update_with_buffer(&value_buffer.iter().map(|value| value.value as u32).collect::<Vec<u32>>(), WIDTH, HEIGHT).unwrap();
+        }
+        else{
+            my_window.update_with_buffer(&value_buffer.iter().map(|value| value.value as u32).collect::<Vec<u32>>(), WIDTH, HEIGHT).unwrap();
+            thread::sleep(time::Duration::from_micros(16600));
+        }        
     }
-
-
-
-
-    //fractal::compute_iterations(&mut Vec2::new_point(0., 0.), &mut Vec2::new_point(-0.5, 0.5), 50);
 }
